7ba655cd1129e07daa347ab85498219ebc7c69b7
Samirkhairati <samirk10101@gmail.com>
2024-06-04 09:18:23 +0530

    build: Scripts setup for build commands

--- server/dist/index.js
+++ server/dist/index.js
@@ -3,15 +3,52 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// PACKAGES
 const express_1 = __importDefault(require("express"));
-const port = 8000;
+// import dotenv from 'dotenv';
+// import cookieParser from 'cookie-parser';
+// import cors from 'cors';
+const path_1 = __importDefault(require("path"));
+// UTILS
+// import connectDB from './config/db.js';
+// import userRoutes from './routes/userRoutes.js';
+// import categoryRoutes from './routes/categoryRoutes.js';
+// import productRoutes from './routes/productRoutes.js';
+// import uploadRoutes from "./routes/uploadRoutes.js";
+// import orderRoutes from './routes/orderRoutes.js';
+// import cloudinarySetup from './config/cloudinary.js';
+// SETUP
+// dotenv.config();
+// connectDB();
+// cloudinarySetup();
 const app = (0, express_1.default)();
-app.get("/", (req, res) => {
-    res.send("HELLO FROM EXPRESS + TS!!!!");
+const location = path_1.default.resolve();
+// MIDDLEWARE
+app.use(express_1.default.json());
+// app.use(cookieParser());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cors(
+//     {
+//         origin: true,
+//         credentials: true,
+//     }
+// ));
+// ROUTES
+// app.use("/api/users", userRoutes);
+// app.use("/api/category", categoryRoutes);
+// app.use("/api/products", productRoutes);
+// app.use("/api/upload", uploadRoutes);
+// app.use("/api/orders", orderRoutes);
+// app.use("/api/upload", uploadRoutes);
+// BUILD
+app.use(express_1.default.static(path_1.default.join(location, '../client/dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path_1.default.join(location, '../client/dist/index.html'));
 });
-app.get("/hi", (req, res) => {
-    res.send("BYEEE!!");
+// TEST
+app.get('/', (req, res) => {
+    res.send(`API is running on port ${process.env.PORT}...`);
 });
-app.listen(port, () => {
-    console.log(`now listening on port ${port}`);
+app.listen(process.env.PORT || 6969, () => {
+    console.log(`Server running on port ${process.env.PORT || 6969}`);
 });
--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,56 @@
-import express, { Express, Request, Response } from "express";
-const port = 8000;
+// PACKAGES
+import express from 'express';
+// import dotenv from 'dotenv';
+// import cookieParser from 'cookie-parser';
+// import cors from 'cors';
+import path from 'path';
 
-const app: Express = express();
+// UTILS
+// import connectDB from './config/db.js';
+// import userRoutes from './routes/userRoutes.js';
+// import categoryRoutes from './routes/categoryRoutes.js';
+// import productRoutes from './routes/productRoutes.js';
+// import uploadRoutes from "./routes/uploadRoutes.js";
+// import orderRoutes from './routes/orderRoutes.js';
+// import cloudinarySetup from './config/cloudinary.js';
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("HELLO FROM EXPRESS + TS!!!!");
+// SETUP
+// dotenv.config();
+// connectDB();
+// cloudinarySetup();
+const app = express();
+const location = path.resolve();
+
+// MIDDLEWARE
+app.use(express.json());
+// app.use(cookieParser());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cors(
+//     {
+//         origin: true,
+//         credentials: true,
+//     }
+// ));
+
+// ROUTES
+// app.use("/api/users", userRoutes);
+// app.use("/api/category", categoryRoutes);
+// app.use("/api/products", productRoutes);
+// app.use("/api/upload", uploadRoutes);
+// app.use("/api/orders", orderRoutes);
+// app.use("/api/upload", uploadRoutes);
+
+// BUILD
+app.use(express.static(path.join(location, '../client/dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(location, '../client/dist/index.html'));
 });
 
-app.get("/hi", (req: Request, res: Response) => {
-  res.send("BYEEE!!");
-});
+// TEST
+app.get('/', (req, res) => {
+    res.send(`API is running on port ${process.env.PORT}...`);
+})
 
-app.listen(port, () => {
-  console.log(`now listening on port ${port}`);
+app.listen(process.env.PORT || 6969, () => {
+    console.log(`Server running on port ${process.env.PORT || 6969}`);
 });
